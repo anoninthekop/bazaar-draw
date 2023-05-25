e3db5c3463779b835c797d2a256af7df29cc3b8e
anoninthekop <41584769+anoninthekop@users.noreply.github.com>
2023-05-25 11:53:47 +0500

    Fixed styling issue

--- index.js
+++ index.js
@@ -17,26 +17,50 @@ function rollClick() {
   winnerEl.classList.add("hide");
   namesEl.classList.remove("hide");
 
+  // setDeceleratingTimeout(randomName, 10, 30);
   var intervalId = window.setInterval(function(){
     console.log("test");
     randomName();
-    namesEl.classList.add("hide");
-    winnerEl.classList.remove("hide");
-    rollAgainEl.classList.remove("hide");
-  
-    const winner = namesEl.innerText;
-    winnerEl.innerText = winner;
-    winnerEl.innerHTML = `<span>And the winner is...</span><br>${winner}`;
   }, 100);
 
   var keyEvent = function (e) {
     console.log(e.which);
     if (e.which == 32) {
         clearInterval(intervalId);
+        namesEl.classList.add("hide");
+        winnerEl.classList.remove("hide");
+        rollAgainEl.classList.remove("hide");
+      
+        const winner = namesEl.innerText;
+        winnerEl.innerText = winner;
+        winnerEl.innerHTML = `<span>And the winner is...</span><br>${winner}`;
         console.log(stop)
     }
   };
   window.addEventListener('keypress', keyEvent);
 
+  // setTimeout(() => {
+  //   namesEl.classList.add("hide");
+  //   winnerEl.classList.remove("hide");
+  //   rollAgainEl.classList.remove("hide");
+
+  //   const winner = namesEl.innerText;
+  //   winnerEl.innerText = winner;
+  //   winnerEl.innerHTML = `<span>And the winner is...</span><br>${winner}`;
+  // }, 4000);
+
 }
 window.rollClick = rollClick;
+
+// function setDeceleratingTimeout(callback, factor, times) {
+//   const internalCallback = ((t, counter) => {
+//     return () => {
+//       if (--t > 0) {
+//         setTimeout(internalCallback, ++counter * factor);
+//         callback();
+//       }
+//     };
+//   })(times, 0);
+
+//   setTimeout(internalCallback, factor);
+// }
